17f16cd4767071f18943f0f15c7bc26e3d73b036
Lisa Nguyen <ngocanhthu.work@gmail.com>
2025-06-15 15:57:13 -0400

    Improve Responsiveness (Version IX)

--- src/listing-details/components/ImageGallery.jsx
+++ src/listing-details/components/ImageGallery.jsx
@@ -9,14 +9,14 @@ import {
 
 function ImageGallery({itemDetail}) {
   return (
-    <div className="px-1 mx-7 mt-3 lg:my-7 h-fit lg:h-112 flex items-center justify-center">
+    <div className="px-1 mx-7 mt-3 lg:my-7 h-fitlg:h-112 flex items-center justify-center">
 
           <Carousel className="size-fit flex items-center">
           <CarouselContent>
             {itemDetail?.images.length > 0 ? itemDetail.images.map((image, index) => (
               <CarouselItem key={index} className="size-full">
                 <img src={image.imageUrl} 
-                  className="size-fit object-contain rounded-2xl my-1 md:my-5" />
+                  className="py-1 object-contain rounded-2xl md:py-5" />
               </CarouselItem>
             )):
             <CarouselItem className='h-[500px] bg-slate-200 animate-pulse'></CarouselItem>
